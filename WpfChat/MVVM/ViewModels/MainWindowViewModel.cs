1d844d4d9da1de894d3507ae4b5cf810a29f0c6b
Nick Fletcher <nickeax@gmail.com>
2024-11-28 16:02:06 +1100

    Setup MainWindow and basic MVVM.

---
+++ WpfChat/MVVM/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,15 @@
+﻿using WpfChat.Client.MVVM.Core;
+using WpfChat.Client.Net;
+
+namespace WpfChatClient.MVVM.ViewModels;
+public class MainWindowViewModel
+{
+    public RelayCommand ConnectToServerCommand { get; set; }
+    Server _server;
+
+    public MainWindowViewModel()
+    {
+        _server = new Server();
+        ConnectToServerCommand = new RelayCommand(o => _server.ConnectToServer());
+    }
+}
---
+++ WpfChat/Net/Server.cs
@@ -0,0 +1,19 @@
+﻿using System.Net.Sockets;
+
+namespace WpfChat.Client.Net;
+public class Server
+{
+    TcpClient _client;
+    public Server()
+    {
+        _client = new TcpClient();
+    }
+
+    public void ConnectToServer()
+    {
+        if (!_client.Connected)
+        {
+            _client.Connect("127.0.0.1", 10_000);
+        }
+    }
+}
